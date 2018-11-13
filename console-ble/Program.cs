6bcf20b18a4b1d96e08a5d63e3c487aa68e4443d
George Fedoseev <george.fedoseev@me.com>
2018-11-13 14:02:33 +0400

    init

---
+++ console-ble/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+
+namespace console_ble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Searching for GearVR controllers...");
+            var controllers = GearVRController.FindGearVRControllers();
+            Console.WriteLine($"Found {controllers.Count} controller(-s)");
+
+            var c = controllers.First();
+            c.ConnectAsync();
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
