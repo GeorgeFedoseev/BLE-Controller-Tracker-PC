34a17ce549a22af3670fdcf78a76266cc845f6f9
George Fedoseev <george.fedoseev@me.com>
2018-11-14 17:41:48 +0400

    added OSC transmitter

---
+++ GearVR-Controller-To-Win/OSCTransmitter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gearvr_controller_to_win
+{
+    class OSCTransmitter
+    {
+        private List<GearVRController> _controllers;
+
+        public OSCTransmitter(List<GearVRController> controllers) {
+            _controllers = controllers;
+        }
+
+        public void Start() {
+            // start transmitting data
+            foreach (var c in _controllers) {
+                
+                c.OnSensorDataUpdated += (sender) => {
+                    TransmitDataForController(sender);
+                };
+                
+            }
+        }
+
+        private void TransmitDataForController(GearVRController controller) {
+
+        }
+    }
+}
