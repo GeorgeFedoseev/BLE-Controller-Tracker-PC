26c387f8f563f76971ea036b908836bb952bfd50
George Fedoseev <george.fedoseev@me.com>
2018-11-14 11:39:56 +0400

    power off controller on dispose - more stable connection after

--- console-ble/GearVRController.cs
+++ console-ble/GearVRController.cs
@@ -12,60 +12,102 @@ using Windows.Devices.Enumeration;
 
 namespace console_ble
 {
-    class GearVRController
+    class GearVRController: IDisposable
     {
 
         static float SEARCH_TIME_SECONDS = 3;
+        static float NO_DATA_CONNECTED_THRESHOLD_SECONDS = 5f;
 
         static Guid UUID_CUSTOM_SERVICE = Guid.Parse("4f63756c-7573-2054-6872-65656d6f7465");
         static Guid UUID_WRITE_CHARACTERISTIC = Guid.Parse("c8c51726-81bc-483b-a052-f7a14ea3d282");
         static Guid UUID_NOTIFY_CHARACTERISTIC = Guid.Parse("c8c51726-81bc-483b-a052-f7a14ea3d281");
 
+        static short CMD_OFF = 0x0000;
+        static short CMD_SENSOR = 0x0100;
+        static short CMD_UNKNOWN_FIRMWARE_UPDATE_FUNC = 0x0200;
+        static short CMD_CALIBRATE = 0x0300;
+        static short CMD_KEEP_ALIVE = 0x0400;
+        static short CMD_UNKNOWN_SETTING = 0x0500;
+        static short CMD_LPM_ENABLE = 0x0600;
+        static short CMD_LPM_DISABLE = 0x0700;
+        static short CMD_VR_MODE = 0x0800;
 
         private string _winDeviceId;
         private BluetoothLEDevice _bleDevice;
 
         private GattCharacteristic _notifyCharacteristic, _writeCharacteristic;
 
+        private volatile bool _wantToConnect = false;
+
         private volatile bool _connected = false;
+        private volatile bool _connectionInProgress = false;
+
+        private DateTime _lastTimeReceivedDataFromController = DateTime.MinValue;
+
+        private Thread _monitorThread;
         
 
         public GearVRController() {
+            // start MonotorThread
+            _monitorThread = new Thread(MonitorThreadWorker);
+            _monitorThread.IsBackground = true;
+            _monitorThread.Start();
         }
 
-        public bool Connect() {
+        // PUBLIC
+        public void Connect()
+        {
+            _wantToConnect = true;
+        }
 
-            bool connected = false;
+        public void Disconnect() {
+            _wantToConnect = false;
+            _Disconnect();
+        }
 
-            while (!connected) {
-                Console.WriteLine("Try connect...");
-                var res = Ping();
+        // MONITOR
+
+        void MonitorThreadWorker() {
+            Console.WriteLine("Monitor thread started");
+
+            while (true) {
+
+                if (_connected && (DateTime.Now - _lastTimeReceivedDataFromController).TotalSeconds > NO_DATA_CONNECTED_THRESHOLD_SECONDS) {
+                    // not receiving data - connection lost
+                    Console.WriteLine($"Didn't receive data for {NO_DATA_CONNECTED_THRESHOLD_SECONDS} seconds - disconnected");
+                    _Disconnect();
+                }
+
+                if (!_connected && _wantToConnect & !_connectionInProgress) {
+                    _Connect();
+                }
+            }
+        }
+
+
+        // CONNECTION
+
+
+        private bool _Connect() {
+            if (_connectionInProgress) {
+                Console.WriteLine("Cant start connecting - connection already in progress");
+                return false;
+            }
+
+            _connectionInProgress = true;
+
+            // untill reach
+            while (true) {
+                Console.WriteLine("Trying to connect...");
+                var res = TryGetGattServices();
 
                 if (res.Status == GattCommunicationStatus.Success) {
                     break;
                 }
 
-                Thread.Sleep(1000);
+                Thread.Sleep(3000);
             }
-
-           // StartKeepAlive();
-
-            //Console.WriteLine($"Connection status: {_bleDevice.ConnectionStatus}");
-            //if (_bleDevice.ConnectionStatus == BluetoothConnectionStatus.Connected) {
-            //    StartKeepAlive();
-            //}
-            //else {
-            //    Console.WriteLine("Not connected, waiting for connection...");
-            //    _bleDevice.ConnectionStatusChanged += OnConnectionAppeared;
-            //    Ping();
-            //}
-
-
-
-            //while (_keepAliveThread == null) {
-            //    Thread.Sleep(100);
-            //}
-
+         
             Console.WriteLine($"Device connection status: {_bleDevice.ConnectionStatus}");
 
 
@@ -76,6 +118,7 @@ namespace console_ble
             
             GattDeviceService controllerService = services.Where(x => x.Uuid == UUID_CUSTOM_SERVICE).FirstOrDefault();
             if (controllerService == null) {
+                _connectionInProgress = false;
                 return false;
             }
 
@@ -88,7 +131,9 @@ namespace console_ble
             Console.WriteLine($"Getting write characteristic success: {getWriteCharacteristicResult.Status}");
 
             if (getWriteCharacteristicResult.Status != GattCommunicationStatus.Success
-                || getNotifyCharacteristicResult.Status != GattCommunicationStatus.Success) {
+                || getNotifyCharacteristicResult.Status != GattCommunicationStatus.Success) 
+            {
+                _connectionInProgress = false;
                 return false;
             }
 
@@ -108,79 +153,52 @@ namespace console_ble
                 else {
                     
                     Console.WriteLine($"Failed to subscribe to characteristic: {result}");
+                    _connectionInProgress = false;
                     return false;
                 }
 
-                // Console.WriteLine($"WriteClientCharacteristicConfigurationDescriptorAsync success: {success}");
-
-                
-                var success = InitialKickEvents().GetAwaiter().GetResult();
+                var success = RequestSensorData().GetAwaiter().GetResult();
                 Console.WriteLine($"Kick Events success: {success}");
 
                 success = SetKeepAlive().GetAwaiter().GetResult();
                 Console.WriteLine($"KeepAlive success: {success}");
-
-
+                
             }
             catch (Exception ex) {
                 // This usually happens when not all characteristics are found
                 // or selected characteristic has no Notify.
                 Console.WriteLine($"Subscribing Exception: {ex.Message}");
+                _connectionInProgress = false;
                 return false;
             }
 
+            _connectionInProgress = false;
             return true;
         }
-
-
-     
-
-        private GattDeviceServicesResult Ping()
+        
+        // ping to BLE device
+        private GattDeviceServicesResult TryGetGattServices()
         {
-            Console.WriteLine("Pinging...");
+            Console.WriteLine("Trying to get GATT services...");
             //return  _bleDevice.GetGattServicesAsync(BluetoothCacheMode.Uncached).AsTask().GetAwaiter().GetResult();
             _bleDevice = BluetoothLEDevice.FromIdAsync(_winDeviceId).AsTask().GetAwaiter().GetResult();
             Console.WriteLine("Got BLE device");
             var res = _bleDevice.GetGattServicesAsync(BluetoothCacheMode.Uncached).AsTask().GetAwaiter().GetResult();
-            Console.WriteLine($"Ping result: {res.Status}");
+            Console.WriteLine($"Result: {res.Status}");
+
+            if (res.Status == GattCommunicationStatus.Unreachable) {
+                _bleDevice.Dispose();
+                _bleDevice = null;
+            }
+
             return res;
         }
-
-
-        //private void StartKeepAlive() {
-            
-
-        //    _keepAliveThread = new Thread(async () => {
-        //        Console.WriteLine("Start KeepAlive");
-
-        //        while (true) {
-        //            var getServicesResult = Ping();
-        //            Console.WriteLine($"KeepAlive result: {getServicesResult.Status}");
-                    
-        //            Thread.Sleep(1000);
-        //        }
-        //    });
-        //    _keepAliveThread.IsBackground = true;
-        //    _keepAliveThread.Start();
-            
-        //}
-
-        //private void StopKeepAlive() {
-        //    if (_keepAliveThread != null) {
-        //        _keepAliveThread.Abort();
-        //        _keepAliveThread = null;
-        //    }
-        //}
-
-        private void _notifyCharacteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
-        {
-            Console.WriteLine("changed");
-        }
-
-        private async Task<bool> InitialKickEvents()
+        
+        // COMANDS FOR GearVR Controller
+        private async Task<bool> RequestSensorData()
         {
             var writer = new Windows.Storage.Streams.DataWriter();
-            short val = 0x0100;
+            short val = CMD_SENSOR;
             writer.WriteInt16(val);
             GattCommunicationStatus writeResult = await _writeCharacteristic.WriteValueAsync(writer.DetachBuffer());
 
@@ -191,7 +209,7 @@ namespace console_ble
         private async Task<bool> SetKeepAlive()
         {
             var writer = new Windows.Storage.Streams.DataWriter();
-            short val = 0x0400;
+            short val = CMD_KEEP_ALIVE;
             writer.WriteInt16(val);
             GattCommunicationStatus writeResult = await _writeCharacteristic.WriteValueAsync(writer.DetachBuffer());
 
@@ -199,11 +217,57 @@ namespace console_ble
             return success;
         }
 
+        private async Task<bool> SendPowerOff()
+        {
+            var writer = new Windows.Storage.Streams.DataWriter();
+            short val = CMD_OFF;
+            writer.WriteInt16(val);
+            GattCommunicationStatus writeResult = await _writeCharacteristic.WriteValueAsync(writer.DetachBuffer());
+
+            bool success = writeResult == GattCommunicationStatus.Success;
+            return success;
+        }
+
+        private void _Disconnect() {
+            if (_connected) {
+                SendPowerOff().GetAwaiter().GetResult();
+            }
+            _connected = false;
+            if (_notifyCharacteristic != null) {
+                _notifyCharacteristic.ValueChanged -= _notifyCharacteristic_ValueChanged;
+                _notifyCharacteristic = null;
+                _writeCharacteristic = null;
+                _bleDevice.Dispose();
+                _bleDevice = null;
+            }
+        }
+
+
+        // ENVENTS
+        private void _notifyCharacteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            _lastTimeReceivedDataFromController = DateTime.Now;
+            _connected = true;
+            Console.WriteLine($"changed {DateTime.Now}");
+        }
+
+        public void Dispose()
+        {
+            if (_connected) {
+                _Disconnect();               
+
+            }
+            
+        }
+
+
+
+        // STATIC
+
         public static async Task<List<GearVRController>> FindPairedGearVRControllersAsync()
         {
             var result = new List<GearVRController>();
-
-          
+            
             var devices = await DeviceInformation.FindAllAsync(GattDeviceService.GetDeviceSelectorFromUuid(UUID_CUSTOM_SERVICE), null);
 
             foreach (var device in devices) {
@@ -213,10 +277,9 @@ namespace console_ble
                 result.Add(controller);
             }
 
-
             return result;
         }
 
-        
+       
     }
 }
--- console-ble/Program.cs
+++ console-ble/Program.cs
@@ -1,38 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Windows.Devices.Bluetooth;
 
 namespace console_ble
 {
     class Program
     {
+        [DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+
+        private delegate bool EventHandler(CtrlType sig);
+        static EventHandler _handler;
+
+        enum CtrlType
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+
+
+        static List<GearVRController> _controllers;
+
         static void Main(string[] args)
         {
+            _handler += new EventHandler(ConsoleExitHandler);
+            SetConsoleCtrlHandler(_handler, true);
+
+
             Console.WriteLine("Searching for GearVR controllers...");
-            var controllers = GearVRController.FindPairedGearVRControllersAsync().GetAwaiter().GetResult();
-            Console.WriteLine($"Found {controllers.Count} controller(-s)");
+            _controllers = GearVRController.FindPairedGearVRControllersAsync().GetAwaiter().GetResult();
+            Console.WriteLine($"Found {_controllers.Count} controller(-s)");
 
-            if (controllers.Count > 0) {
-                var c = controllers.First();
-
-                while (true) {
-                    Console.WriteLine("Try connecting...");
-                    
-                    var connected = c.Connect();
-                    if (connected) {
-                        break;
-                    }
-
-                    Thread.Sleep(1000);
-                }                
+            if (_controllers.Count > 0) {
+                var c = _controllers.First();
+                
+                c.Connect();
+                
+                              
             }
 
             Console.ReadKey();
         }
+
+
+        private static bool ConsoleExitHandler(CtrlType sig)
+        {
+            Console.WriteLine("Exiting system due to external CTRL-C, or process kill, or shutdown");
+
+            _controllers.ForEach(c => c.Dispose());
+
+            //do your cleanup here
+            //Thread.Sleep(5000); //simulate some cleanup delay
+
+            Console.WriteLine("Cleanup complete");
+
+            //allow main to run off
+            //exitSystem = true;
+
+            //shutdown right away so there are no lingering threads
+            Environment.Exit(-1);
+
+            return true;
+        }
+
 
 
     }
